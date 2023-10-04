fdd6746e856e427b4509edc4f8be6bd768f707dc
sjMalik <surajit94malik@gmail.com>
2023-10-04 13:32:38 +0530

    swagger ui

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerJsDocs = YAML.load('./api.yaml');
 
 const app = express();
 
@@ -8,15 +12,17 @@ require('dotenv').config();
 const indexRouter = require('./routes');
 const authorRouter = require('./routes/author');
 
-mongoose.connect(process.env.MONGO_URI, {  useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => {
         console.log('MongoDb connected')
     })
     .catch((e) => {
         console.log(e)
-})
+    })
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
 
